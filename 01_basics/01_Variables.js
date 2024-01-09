bfe540af7856bf927366fe9d0662153afb036750
diwakargupta077 <156025612+diwakargupta077@users.noreply.github.com>
2024-01-09 02:27:30 +0000

    datatypes

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,18 @@
+const platform = "e-commerce";
+let name = "Diwakar Gupta";
+var cityName = "Parasi";
+let preferedLocation;
+//platform = "e-learning";
+console.log(platform);
+
+/* prefer not to use Var 
+Because of the issue of block and functional scope    i.e braces{}
+
+*/
+
+
+name = "Kaushal Bhandari";
+cityName = "Butwal";
+console.table([platform,name, cityName,preferedLocation]);
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+/*  there are a lot of things added into JS nowadays so to declare the code you are writing as a newer version we use (strict use) 
+in double quote and if it is declared once there is no way to return back all the codes will be treated as newer version's one or  can be said as newer syntaxes */
+
+"use strict";
+
+// alert(3+3) we cannot use alert into node as it is used in the browser it has some other syntax in node.js
+                   //Primitive DataTypes
+//types of data
+// number
+// bigint => for storing large values
+//string 
+//boolean => true or false
+//null => it is a stand alone value
+//undefined => default value when some value is not assigned
+//symbol => used while talking about uniqueness, mostly used in REACT AND FIGMA TOOLS
+                             
+                             //Special datatypes
+//objects
+
+
+
+console.log(typeof null);  //output is "object" which is one of the major reason why there cause more errors in javascript
+console.log(typeof undefined);   // output is undefined
+
